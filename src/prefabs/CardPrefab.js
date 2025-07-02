6da908b3c9f00c42ff41e02894983de38074c7ca
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2025-07-02 09:47:58 +0100

    add timeline card flip tween, only back texture so far

--- src/prefabs/CardPrefab.js
+++ src/prefabs/CardPrefab.js
@@ -11,8 +11,34 @@ export default class CardPrefab extends Phaser.GameObjects.Image {
 	constructor(scene, x, y, texture, frame) {
 		super(scene, x ?? 0, y ?? 0, texture || "spritesheet", frame ?? "Verse.png");
 
+		this.setInteractive(new Phaser.Geom.Rectangle(0, 0, 144, 144), Phaser.Geom.Rectangle.Contains);
+
 		/* START-USER-CTR-CODE */
-		// Write your code here.
+		this.on(Phaser.Input.Events.POINTER_DOWN, () => {
+         console.log('clicked card')
+         this.flipTimeLine.play(true);
+      });
+      this.flipTimeLine = this.scene.add.timeline([
+         {
+            tween: {
+               targets: this,
+               scaleX: 0,
+               scaleY: 1.2,
+               duration: 100,
+               ease: Phaser.Math.Easing.Linear
+            }
+         },
+         {
+            at: 100,
+            tween: {
+               targets: this,
+               scaleX: 1,
+               scaleY: 1,
+               duration: 100,
+               ease: Phaser.Math.Easing.Linear
+            }
+         }
+      ]);
 		/* END-USER-CTR-CODE */
 	}
 
@@ -24,6 +50,7 @@ export default class CardPrefab extends Phaser.GameObjects.Image {
 	/* START-USER-CODE */
 
 	// Write your code here.
+   flipTimeLine;
 
 	/* END-USER-CODE */
 }
